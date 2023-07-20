ce668e01abc5ae3b1eb1f5af27541293356db2e9
Chitra <126138761+Chitra-P-99@users.noreply.github.com>
2023-07-20 12:37:49 +0530

    Add files via upload

---
+++ src/components/secnavbar.jsx
@@ -0,0 +1,20 @@
+
+import '../styles/secnavbar.css';
+const Secnavbar=()=>{
+    return(
+
+        <div className="secnavbar">
+         <ul>
+        <li><a href="">Movies</a></li>
+        <li><a href="">Streams</a></li>
+        <li><a href="">Events</a></li>
+        <li><a href="">Plays</a></li>
+        <li><a href="">Sports</a></li>
+        <li><a href="">Activities</a></li>
+        <li><a href="">Buzz</a></li>
+        </ul>
+
+        </div>
+    )
+}
+export default Secnavbar;
